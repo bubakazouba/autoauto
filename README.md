62e3a1446240f2db8bd2927dc354091548de4cb3
bubakazouba <slashhashdash@gmail.com>
2021-07-06 18:35:34 -0700

    adds host

---
+++ README.md
@@ -0,0 +1,5 @@
+## Installation
+* `cp  host/com.my_company.my_app.json ~/Library/Application Support/Microsoft Edge/NativeMessagingHosts`
+* Modify extension id and `host.py` path if needed
+* Make sure `host.py` is executable
+* `pip install -r requirements.txt`
---
+++ host/host.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import nativemessaging
+import keyboard
+import json
+import time
+
+def getKeyboardStringFromKeyParams(keyParams):
+    s = keyParams["key"]
+    if s[:5] == "Arrow":
+        s = s[5:]
+    if s[:4] == "Meta":
+        return ""
+    if keyParams["metaKey"]:
+        s = "cmd+" + s
+    return s
+
+while True:
+    message = nativemessaging.get_message()
+    if message == "hello":
+        nativemessaging.send_message(nativemessaging.encode_message("I'm alive"))
+    else:
+        nativemessaging.send_message(nativemessaging.encode_message("hi im still ok" + str(len(message))))
+        keys = json.loads(message)
+        for key in keys:
+            time.sleep(0.5)
+            s = getKeyboardStringFromKeyParams(key["action"]["keyParams"])
+            if s == "":
+                nativemessaging.send_message(nativemessaging.encode_message("skipping cuz empty"))
+                continue
+            keyboard.press_and_release(s)
+            nativemessaging.send_message(nativemessaging.encode_message(s))
+        nativemessaging.send_message(nativemessaging.encode_message("done"))
