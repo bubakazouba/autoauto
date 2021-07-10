a11935b4c4d0a6e0c13a98062acf88d10624714a
bubakazouba <slashhashdash@gmail.com>
2021-07-09 19:39:18 -0700

    temp

---
+++ host/adhoc.py
@@ -0,0 +1,26 @@
+def get_all_possible_start_indices_for_k(n ,k):
+	l = []
+	for i in range(n-k): # all possible k
+		for j in range(i+k, n-k):
+			l.append((i,j))
+	return l
+
+def array_equal(actions1, actions2):
+	return actions1 == actions2
+
+def detect_repition(actions):
+	n = len(actions)
+	for k in range(int(n/2),-1,-1): # max k is n/2
+		print("k=", k)
+		if k ==2:
+			break
+		all_possible_start_indices_for_k = get_all_possible_start_indices_for_k(n, k)
+		print(all_possible_start_indices_for_k)
+		for ij in all_possible_start_indices_for_k:
+			i = ij[0]
+			j = ij[1]
+			if array_equal(actions[i: i+k], actions[j: j+k]):
+				print("chose i,j=", i,j)
+				print("going to return", [x["key"] for x in actions[i:j]])
+				return actions[i:j]
+	return []
---
+++ host/tst/adhoc_test.py
@@ -0,0 +1,14 @@
+import unittest
+from context import adhoc
+import testutils
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_markov_chain_0(self):
+        actionsList = testutils.jsonLoad("markov_test_cases.json")
+        for idx, actions in enumerate(actionsList):
+            output = adhoc.detect_repition(actions)
+            self.assertEqual(output, testutils.jsonLoad("markov_expected.json")[idx])
+
+if __name__ == '__main__':
+    unittest.main()
