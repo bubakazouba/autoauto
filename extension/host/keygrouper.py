172131b9eb028db6fb53dc6e6bb057c1ecee9fe4
bubakazouba <slashhashdash@gmail.com>
2021-07-25 12:22:34 -0700

    Allows keygrouper to take in an initialValue

--- extension/host/keygrouper.py
+++ extension/host/keygrouper.py
@@ -1,10 +1,11 @@
 class Part:
-    def __init__(self, start, end, _id, pastedText):
+    # text could be pasted text or initial value
+    def __init__(self, start, end, _id, text):
         self.start = start
         self.end = end
-        self.raw_end = len(pastedText) if end == "E" else end
+        self.raw_end = len(text) if end == "E" else end
         self.id = _id
-        self.pastedText = pastedText
+        self.text = text
 
     def indicesWithinBoundaries(self):
         outOfBounds = self.start < 0 or self.raw_end < 0 or self.raw_end <= self.start
@@ -13,9 +14,9 @@ class Part:
     def getVal(self):
         if not self.indicesWithinBoundaries():
             return ""
-        return self.pastedText[self.start:self.raw_end]
+        return self.text[self.start:self.raw_end]
     def __str__(self):
-        return str([self.start,self.end,self.raw_end])
+        return str([self.start,self.end,self.raw_end, self.text])
     def __eq__(self, other):
         """Overrides the default implementation"""
         if isinstance(other, Part):
@@ -23,7 +24,7 @@ class Part:
             self.end == other.end and \
             self.raw_end == other.raw_end and \
             self.id == other.id and \
-            self.pastedText == other.pastedText
+            self.text == other.text
         return False
     def __ne__(self, other):
         """Overrides the default implementation (unnecessary in Python 3)"""
@@ -31,9 +32,14 @@ class Part:
     def __hash__(self):
         """Overrides the default implementation"""
         return hash(tuple(sorted(self.__dict__.items())))
+
 class KeyGrouper:
-    def __init__(self):
+    def __init__(self, initialValue=""):
+        self.initiaValueWasSet = False
         self.parts = []
+        if len(initialValue) > 0:
+            self.initiaValueWasSet = True
+            self.parts = [Part(0, "E", "INITIAL_VALUE", initialValue)]
     
     def getVal(self):
         val = ""
@@ -71,6 +77,12 @@ class KeyGrouper:
     def getLenPart(self, i):
         return len(self.getValForPart(self.parts[i]))
     
+    # If initialValue is here i can check how much of it was removed/split..etc
+    # if its not there we cant tell if it wasnt there from the first place or if it was there
+    # then removed
+    def wasInitialValueRemoved(self):
+        return not any([type(p) == Part and p.id == "INITIAL_VALUE" for p in self.parts]) and \
+            self.initiaValueWasSet
     def getPartToUpdateOrIndexToInsertNewPart(self, offset):
         currentOffset = 0
         for i in range(len(self.parts)):
@@ -81,13 +93,16 @@ class KeyGrouper:
             elif currentOffset > offset:
                 return self.getLenPart(i) - (currentOffset- offset), i
         return None, currentOffset
-
     def appendTextAtOffset(self, char, offset):
+        for c in reversed(char):
+            self._appendTextAtOffset(c, offset)
+    def _appendTextAtOffset(self, char, offset):
         indexInsidePart, index = self.getPartToUpdateOrIndexToInsertNewPart(offset)
-        if indexInsidePart is not None:
+        # if indexInsidePart is 0 then its the beginning of the part, we can just append it before it
+        if indexInsidePart is not None and indexInsidePart != 0:
             initialPart = self.parts[index]
-            firstPart = Part(initialPart.start, indexInsidePart, initialPart.id, initialPart.pastedText)
-            secondPart = Part(indexInsidePart, initialPart.end, initialPart.id, initialPart.pastedText)
+            firstPart = Part(initialPart.start, indexInsidePart, initialPart.id, initialPart.text)
+            secondPart = Part(indexInsidePart, initialPart.end, initialPart.id, initialPart.text)
             self.parts[index] = firstPart
             self.parts.insert(index + 1, char)
             self.parts.insert(index + 2, secondPart)
@@ -104,9 +119,9 @@ class KeyGrouper:
         indexInsidePart, index = self.getPartToUpdateOrIndexToInsertNewPart(offset)
         if indexInsidePart is not None:
             initialPart = self.parts[index]
-            indexInsidePart += initialPart.start+1
-            firstPart = Part(initialPart.start, indexInsidePart - 1, initialPart.id, initialPart.pastedText)
-            secondPart = Part(indexInsidePart, initialPart.end, initialPart.id, initialPart.pastedText)
+            indexInsidePart += initialPart.start + 1
+            firstPart = Part(initialPart.start, indexInsidePart - 1, initialPart.id, initialPart.text)
+            secondPart = Part(indexInsidePart, initialPart.end, initialPart.id, initialPart.text)
             if len(self.getValForPart(firstPart)) != 0:
                 self.parts[index] = firstPart
             if len(self.getValForPart(secondPart)) != 0:
--- extension/host/tst/keygrouper_test.py
+++ extension/host/tst/keygrouper_test.py
@@ -35,11 +35,8 @@ class TestStringMethods(unittest.TestCase):
         p1 = keygrouper.Part(0, 1, "PASTE_ID_1", "hello")
         p2 = keygrouper.Part(1, "E", "PASTE_ID_1", "hello")
         self.assertEqual(s.getVal(), "xh!elloabdc")
-        resultParts = s.getParts()
         expectedParts = ["x", p1, "!", p2, "abdc"]
-        self.assertEqual(len(resultParts), len(expectedParts))
-        for i in range(len(s.getParts())):
-            self.assertEqual(resultParts[i], expectedParts[i])
+        self.assertPartsEqual(s, expectedParts)
     
     def test_keygrouper_appends_then_appendsPaste_then_appendsMidPaste_thenRemoves(self):
         s = keygrouper.KeyGrouper()
@@ -55,8 +52,37 @@ class TestStringMethods(unittest.TestCase):
         p1 = keygrouper.Part(0, 1, "PASTE_ID_1", "hello")
         p2 = keygrouper.Part(1, 4, "PASTE_ID_1", "hello")
         self.assertEqual(s.getVal(), "xh!ellbdc")
-        resultParts = s.getParts()
         expectedParts = ["x", p1, "!", p2, "bdc"]
+        self.assertPartsEqual(s, expectedParts)
+
+    def test_keygrouper_with_initialValue(self):
+        s = keygrouper.KeyGrouper("initial value")
+        self.assertEqual(s.getVal(), "initial value")
+
+        p0 = keygrouper.Part(0, "E", "INITIAL_VALUE", "initial value")
+        expectedParts = [p0]
+        self.assertPartsEqual(s, expectedParts)
+
+    def test_keygrouper_with_initialValue_and_changes(self):
+        s = keygrouper.KeyGrouper("initial value")
+        
+        s.appendTextAtOffset("a", 0)
+        s.appendTextAtOffset("b", 1)
+        s.appendTextAtOffset("c", 2)
+        s.appendTextAtOffset("d", 2)
+        s.appendTextAtOffset("x", 0)
+        s.appendPasteAtOffset("hello", 1, "PASTE_ID_1")
+        s.appendTextAtOffset("!",2)
+
+        self.assertEqual(s.getVal(), "xh!elloabdcinitial value")
+        p0 = keygrouper.Part(0, "E", "INITIAL_VALUE", "initial value")
+        p1 = keygrouper.Part(0, 1, "PASTE_ID_1", "hello")
+        p2 = keygrouper.Part(1, "E", "PASTE_ID_1", "hello")
+        expectedParts = ["x", p1, "!", p2, "abdc", p0]
+        self.assertPartsEqual(s, expectedParts)
+
+    def assertPartsEqual(self, s, expectedParts):
+        resultParts = s.getParts()
         self.assertEqual(len(resultParts), len(expectedParts))
         for i in range(len(s.getParts())):
             self.assertEqual(resultParts[i], expectedParts[i])
