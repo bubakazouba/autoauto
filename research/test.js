13106376bda2cf579ff71457402d058303b6d324
bubakazouba <slashhashdash@gmail.com>
2021-08-26 22:59:50 -0700

    adds stuff i tried that didnt work for JS dispatching events in sheets

--- research/test.js
+++ research/test.js
@@ -26,3 +26,36 @@ const eventObj = document.createEvent("Event");
 eventObj.initEvent("keypress", true, true);
 eventObj.keyCode = 105;
 input.dispatchEvent(eventObj);
+----------
+// testing spreadsheets inputting
+eEnter = document.createEvent("Event");
+eEnter.initEvent("keydown", true, false);
+eEnter.keyCode = 13; //105
+eEnter.key = "Enter"
+document.getElementsByClassName("cell-input")[0].dispatchEvent(eEnter)
+
+
+var eEnter = new KeyboardEvent("keydown", {bubbles : true, cancelable : true, key : "Enter", code: "Enter", keyCode: 13, shiftKey: false});
+var eEnterPress = new KeyboardEvent("keypress", {bubbles : true, cancelable : true, key : "Enter", code: "Enter", keyCode: 13, shiftKey: false});
+var eEnterUp = new KeyboardEvent("keyup", {bubbles : true, cancelable : true, key : "Enter", code: "Enter", keyCode: 13, shiftKey: false});
+
+var eEscape = new KeyboardEvent("keydown", {bubbles : true, cancelable : true, key : "Escape", code: "Escape", keyCode: 27, shiftKey: false});
+var eEscapeUp = new KeyboardEvent("keyup", {bubbles : true, cancelable : true, key : "Escape", code: "Escape", keyCode: 27, shiftKey: false});
+
+document.getElementsByClassName("cell-input")[1].dispatchEvent(eEnter); // Doesnt work
+document.getElementsByClassName("cell-input")[1].dispatchEvent(eEnterUp); // Doesnt work
+document.getElementsByClassName("cell-input")[1].dispatchEvent(eEnterPress); // Doesnt work
+
+document.getElementsByClassName("cell-input")[1].dispatchEvent(eEscape) // This works!
+document.getElementsByClassName("cell-input")[1].dispatchEvent(eH) // Doesnt work
+
+var eH = new KeyboardEvent("keydown", {bubbles : true, cancelable : true, key : "h", code: "KeyH", keyCode: 72, shiftKey: false});
+//////
+eTry = document.createEvent("KeyboardEvent")
+// eTry.initKeyEvent(type, bubbles, cancelable, viewArg, 
+//        ctrlKeyArg, altKeyArg, shiftKeyArg, metaKeyArg, 
+//            keyCodeArg, charCodeArg)
+// eTry.initKeyEvent("keypress", true, true, null, false, false, false, false, 72, 72);
+eTry.initKeyboardEvent("keydown", true, true, document.defaultView, false, false, false, false, 72, 72);
+eTry.keyCodeVal = 72;
+document.getElementsByClassName("cell-input")[1].dispatchEvent(eTry)
