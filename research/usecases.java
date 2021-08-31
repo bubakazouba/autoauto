d2d25c632b30517f007843f17bea2abd45461731
bubakazouba <slashhashdash@gmail.com>
2021-08-30 20:06:03 -0700

    adds use cases file, renames extension

---
+++ research/usecases.java
@@ -0,0 +1,31 @@
+1- download sheets
+2- conditions 3la 7asab el report
+3- analysis mo3yna a3mlha mra w a-save it
+    3 DBs
+        work force management
+            byreport lel wages/work hours/scheduled hours
+        key performance indicator
+            target 100 items per hour
+            should get flagged
+            lw tele3 < 100, 3ayzen n3rf leh:
+                call someone in the office
+4- reports on who uses which dashboards
+5- bakrah abos 3la arkam
+
+ideas:
+    conditions based on charts
+    save and share workflows across
+        UI for saving and optionally naming
+========================================================
+make kathryn happy:
+    * support right click copy and right click paste (both inside and outside Sheets)
+    * does she need to modify stuff in sheets without a paste? [probably yes :/]
+ask kathryn if she cares about these:
+    cmd+v and cmd+shift+v differentiation https://github.com/bubakazouba/autoauto/issues/69
+    multiple sheets in 1 spreadsheet https://github.com/bubakazouba/autoauto/issues/68
+questions to kathryn:
+    do u only do copies and pastes in the spreadsheet?
+        do you ever manually modify stuff there?
+    mac or windows?
+    what browser?
+>>more manual testing of keygrouper/actiongrouper on text field
